1f09cb261246ce984bb218f48aea4306ebaa3b00
Pavel Emelyanov <ovzxemul@gmail.com>
2018-08-27 20:56:50 +0300

    add skeleton for facebook-auth function
    Not even compile tested yet, as the testing farm is temporarily broken.
    
    Signed-off-by: Pavel Emelyanov <ovzxemul@gmail.com>

---
+++ functions/golang/facebook-user-mgmt.go
@@ -0,0 +1,124 @@
+/*
+ * Facebook account management for swifty Auth-as-a-Service
+ */
+
+package main
+
+import (
+	"fmt"
+	"swifty"
+	"net/http"
+	"encoding/json"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type authResp struct {
+	Error	string	`json:"error,omitempty"`
+	Token	string	`json:"token,omitempty"`
+}
+
+func doFacebookReq(url string) (map[string]string, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Error talking to facebook: %s", err.Error())
+		return nil, err
+	}
+
+	tok, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		fmt.Printf("Error reading resp body: %s", err.Error())
+		return nil, err
+	}
+
+	var tok map[string]string
+
+	err := json.Unmarshal(tok)
+	if err != nil {
+		fmt.Printf("Error unmarshalling resp: %s", err.Error())
+		return nil, err
+	}
+
+	return tok, nil
+}
+
+func getCredsFromFacebook(args map[string]string) (string, error) {
+	url := "https://graph.facebook.com/v3.0/oauth/access_token"?
+	url += "?client_id=" + os.Getenv("ACC_FACEBOOKAAAS_CLIENT")
+	url += "&client_secret=" + os.Getenv("ACC_FACEBOOKAAAS_SECRET")
+	url += "&code=" + args["code"]
+	url += "&redirect_uri=" + args["redirect_uri"]
+
+	tok, err := doFacebookReq(url)
+	if err != nil {
+		return "", err
+	}
+
+	url = "https://graph.facebook.com/v3.0/me"
+	url += "?access_token=" + tok["access-token"]
+	usr, err := doFacebookReq(url)
+	if err != nil {
+		return "", err
+	}
+
+	return usr["id"], nil
+}
+
+func doSignup(auth *swifty.AuthCtx, args map[string]string) interface{} {
+	fbid, err := getCredsFromFacebook(args)
+	if err != nil {
+		return &authResp{Error: "Error talking to facebook"}
+	}
+	err = auth.UsersCol.Insert(bson.M{ "facebookid": fbid })
+	if err != nil {
+		fmt.Printf("Error inserting: %s", err.Error())
+		return &authResp{Error: "Error registering"}
+	}
+
+	return &authResp{}
+}
+
+func doSignin(auth *swifty.AuthCtx, args map[string]string) interface{} {
+	fbid, err := getCredsFromFacebook(args)
+	if err != nil {
+		return &authResp{Error: "Error talking to facebook"}
+	}
+
+	var urec map[string]interface{}
+
+	err := auth.UsersCol.Find(bson.M{"facebookid": fbid}).One(&urec)
+	if err != nil {
+		if err == mgo.ErrNotFound {
+			return &authResp{Error: "Invalid credentials"}
+		}
+
+		fmt.Printf("Error signing up: %s", err.Error())
+		return &authResp{Error: "Error signing in"}
+	}
+
+	jwt, err := auth.MakeJWT(map[string]interface{}{ "facebookid": fbid, "cookie": urec["_id"] })
+	if err != nil {
+		return &authResp{Error: "Error signing in"}
+	}
+
+	return &authResp{Token: jwt}
+}
+
+func Main(req *Request) (interface{}, *Responce) {
+	auth, err := swifty.AuthContext()
+
+	if err != nil {
+		fmt.Println(err)
+		panic("Can't get auth context")
+	}
+
+	switch req.Path {
+	case "signup":
+		return doSignup(auth, req.Args), nil
+	case "signin":
+		return doSignin(auth, req.Args), nil
+	}
+
+	return "Invalid action", nil
+}
